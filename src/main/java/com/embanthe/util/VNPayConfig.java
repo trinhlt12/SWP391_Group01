26db46f153391cd0daf72f376e193bab228b7b5b
trinhltl <lamtrinhh1201@gmail.com>
2025-12-13 18:03:46 +0700

    [Feature] Add `VNPayConfig` class for payment configuration constants and update project SDK configuration

---
+++ src/main/java/com/embanthe/util/VNPayConfig.java
@@ -0,0 +1,8 @@
+package com.embanthe.util;
+
+public class VNPayConfig {
+    public static final String vnp_TmnCode ="RZOERMBX";
+    public static final String vnp_HashSecret = "HV36ENF16B4ZD8MPEG8N6YUTOK0T3N0T";
+    public static final String vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+    public static final String vnp_ReturnUrl = "http://localhost:8080/SWP391_Group01/payment-return";
+}
